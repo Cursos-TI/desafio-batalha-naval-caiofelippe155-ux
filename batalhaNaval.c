76c09587e0ca4a51dda5a1fe8fbe19e21814cee8
caiofelippe155-ux <caiofelippe155@gmail.com>
2026-05-31 04:00:58 +0000

    Eu concluí o desafio do nível mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -13,17 +13,17 @@ int main() {
 
     // Definir as coordenadas iniciais
 
-    int linhaVertical = 2;
-    int colunaVertical = 1;
+    int linhaVertical = 0;
+    int colunaVertical = 9;
 
-    int linhaHorizontal = 5;
-    int colunaHorizontal = 6;
+    int linhaHorizontal = 0;
+    int colunaHorizontal = 5;
 
-    int linhaDiagonal1 = 5;
-    int colunaDiagonal1 = 2;
+    int linhaDiagonal1 = 3;
+    int colunaDiagonal1 = 7;
 
-    int linhaDiagonal2 = 3;
-    int colunaDiagonal2 = 6;
+    int linhaDiagonal2 = 7;
+    int colunaDiagonal2 = 4;
 
     // =========================
     // NAVIO VERTICAL
@@ -148,6 +148,145 @@ int main() {
     } else {
         printf("Posição inválida para o navio diagonal secundário.\n");
     }
+
+
+    // Criar as matrizes das habilidades
+
+    int cone[5][5] = {0};
+    int cruz[5][5] = {0};
+    int octaedro[5][5] = {0};
+
+    // Construir a matriz Cone
+
+    for (int i = 0; i < 5; i++) 
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if (
+                (i == 0 && j == 2) ||
+                (i == 1 && j >= 1 && j <= 3) ||
+                (i == 2)
+            ) {
+                cone[i][j] = 1; 
+            }    
+        }
+                
+    }
+
+    // Construir a matriz Cruz   
+    
+    for (int i = 0; i < 5; i++) 
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if (
+                (i == 2 || j == 2)
+            ) {
+                cruz[i][j] = 1; 
+            }    
+        }
+                
+    } 
+    
+    // Construir a matriz Octaedro
+
+    for (int i = 0; i < 5; i++) 
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if (
+                (i == 0 && j == 2) ||
+                (i == 1 && j >= 1 && j <= 3) ||
+                (i == 2) || 
+                (i == 3 && j >= 1 && j <= 3) || 
+                (i == 4 && j == 2))
+            {
+                octaedro[i][j] = 1; 
+            }    
+        }
+                
+    } 
+    
+    //Definir origem das habilidades
+    
+    int origemConeLinha = 2;
+    int origemConeColuna = 2;
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (cone[i][j] == 1)
+            {
+                int linhaTabuleiro = origemConeLinha + (i - 2);
+                int colunaTabuleiro = origemConeColuna + (j - 2);
+
+                if (
+                    linhaTabuleiro >= 0 &&
+                    linhaTabuleiro < 10 &&
+                    colunaTabuleiro >= 0 &&
+                    colunaTabuleiro < 10 &&
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] == 0
+                )
+                {
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+                }
+            }
+        }
+    }
+
+    int origemCruzLinha = 6;
+    int origemCruzColuna = 7;
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (cruz[i][j] == 1)
+            {
+                int linhaTabuleiro = origemCruzLinha + (i - 2);
+                int colunaTabuleiro = origemCruzColuna + (j - 2);
+
+                if (
+                    linhaTabuleiro >= 0 &&
+                    linhaTabuleiro < 10 &&
+                    colunaTabuleiro >= 0 &&
+                    colunaTabuleiro < 10 &&
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] == 0
+                )
+                {
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+                }
+            }
+        }
+    }    
+
+    int origemOctaedroLinha = 6;
+    int origemOctaedroColuna = 2;
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (octaedro[i][j] == 1)
+            {
+                int linhaTabuleiro = origemOctaedroLinha + (i - 2);
+                int colunaTabuleiro = origemOctaedroColuna + (j - 2);
+
+                if (
+                    linhaTabuleiro >= 0 &&
+                    linhaTabuleiro < 10 &&
+                    colunaTabuleiro >= 0 &&
+                    colunaTabuleiro < 10 &&
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] == 0
+                )
+                {
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+                }
+            }
+        }
+    }    
+
 
     // =========================
     // EXIBIR TABULEIRO
